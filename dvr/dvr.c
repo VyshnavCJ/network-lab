ceb893c42ce39fe60258c3285a46625e524a5ece
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-08-06 21:24:50 +0530

    fix: dvr

--- dvr/dvr.c
+++ dvr/dvr.c
@@ -1,46 +1,44 @@
-#include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-struct node	{
-	unsigned dist[20];
-	unsigned from[20];
-}rt[10];
-int main()	{
-	int costmat[20][20];
-	int nodes,i,j,k,count=0;
-	printf("\nEnter the number of nodes: ");
-	scanf("%d",&nodes);
-	printf("\nEnter the cost matrix\n");
-	for(i=0;i<nodes;i++)	{
-		for(j=0;j<nodes;j++)	{
-			scanf("%d",&costmat[i][j]);
-			costmat[i][i]=0;
-			rt[i].dist[j]=costmat[i][j];
-		rt[i].from[j]=j;
-		}
-	}
+#include<stdio.h>
 
-	do{
-		count=0;
-		for(i=0;i<nodes;i++)	
-		for(j=0;j<nodes;j++)	
-		for(k=0;k<nodes;k++)	
-			if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-			{
-				rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-				rt[i].from[j]=k;
-				count++;
-			}
-	}while(count!=0);
+struct node{
+    int from[20];
+    int dist[20];
+}rt[20];
 
-	for(i=0;i<nodes;i++)
-	{
-		printf("\n\nFOR ROUTER %d\n",i+1);
-		for(j=0;j<nodes;j++)
-		{
-			printf("\t\nnode %d via %d distance %d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
-		}
-	}
-	printf("\n");
+int main(){
+    int n;
+    printf("Enter the no nodes: ");
+    scanf("%d",&n);
+    printf("Enter the distance between nodes: \n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           scanf("%d",&rt[i].dist[j]);
+           rt[i].from[j] = j;
+        }
+    } 
+
+    int count=0;
+    do{
+        count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    if(rt[i].dist[j]>rt[i].dist[k]+rt[k].dist[j]){
+                        rt[i].dist[j] = rt[i].dist[k]+rt[k].dist[j];
+                        rt[i].from[j] = k;
+                        count++;
+                    }
+                }
+            }
+        }
+    }while(count!=0);
+
+    for(int i=0;i<n;i++){
+        printf("\n\nFor router %d distance to\n",i+1);
+        for(int j=0;j<n;j++){
+            printf("%d is %d\n",j+1,rt[i].dist[j]);
+        }
+    }
+
 }
-
