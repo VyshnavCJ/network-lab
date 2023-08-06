0f383bbb877d34acd4cb63ca27208b24b85801ca
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-08-06 20:02:35 +0530

    fix: stop and wait

--- sliding_window/stop_and_wait/sender.c
+++ sliding_window/stop_and_wait/sender.c
@@ -1,53 +1,49 @@
 #include<stdio.h>
-#include<sys/types.h>
 #include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
-
-int main(int argc,char * argv[] ){
-	int sockfd,portno,n;
-	char buffer[255],buffer1[255];
-	struct sockaddr_in serv_addr;
+#include<arpa/inet.h>
+#define PORT 8080
+#define MAX 1000
+#define SA struct sockaddr
+#define SAI struct sockaddr_in
+int main(){
+	int sockfd,n=0;
+	char buffer[MAX];
+	SAI serv_addr;
 	sockfd=socket(AF_INET,SOCK_STREAM,0);
 	
-	portno = atoi(argv[1]);
 	
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(portno);
-	serv_addr.sin_addr.s_addr =INADDR_ANY;
-	//printf("%d",portno);
+	serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_addr.s_addr =htonl(INADDR_ANY);
 	
-	bzero(buffer,255);
-	bzero(buffer1,255);
+	bzero(buffer,MAX);
 	
-	connect(sockfd,(struct sockaddr*)& serv_addr,sizeof(serv_addr));
+	if((connect(sockfd,(struct sockaddr*)& serv_addr,sizeof(serv_addr)))==0)
+        printf("Connected with server..\n");
 	printf("Enter the no of message:");
-	scanf("%s",buffer);
-	n = write(sockfd,buffer,255);
+	while((buffer[n++]=getchar())!='\n');	
+	write(sockfd,buffer,MAX);
 	int no= atoi(buffer);
 	int i =0;
 	while(1){
-	
-		printf(" enter the data ");
-		scanf("%s",buffer);
-		
-	
-		n = write(sockfd,buffer,255);
+        bzero(buffer,MAX);	
+		printf("Enter the data: ");
+        n=0;
+		while((buffer[n++]=getchar())!='\n');	
+		write(sockfd,buffer,MAX);
 		printf("the message is : %s\n",buffer);
-		n = read(sockfd,buffer1,255);
-		printf("the reply is : %s\n",buffer1);
-		if(strcmp("ACKNOWLEDGEMENT",buffer1)==0){
-			i++;
-			
-		}
-		
+        bzero(buffer,MAX);
+		read(sockfd,buffer,MAX);
+		printf("the reply is : %s\n",buffer);
+		if(strcmp("ACKNOWLEDGEMENT",buffer)==0){
+			i++;	
+		}	
 		if(i==no){
 			break;
 		}
 	}
-	
-	n = read(sockfd,buffer1,255);
+    close(sockfd);	
 }
